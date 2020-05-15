deb420b517ecae88247e90073e2ea6dc53332b97
Sumer Cip <sumerc@gmail.com>
2020-05-15 22:47:15 +0300

    update db

--- generate_data.py
+++ generate_data.py
@@ -4,5 +4,7 @@ from django.contrib.auth.models import User
 u = User.objects.get(id=1)
 print(u)
 
-new_comments = [Comment(user=u, comment='Oh! That is awesome!') for i in range(1000)]
+new_comments = []
+for i in range(1000):
+    new_comments.append(Comment(user=u, comment='Oh! That is awesome!'))
 Comment.objects.bulk_create(new_comments)
