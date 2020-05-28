7ac7c1ff693c5a918276d890baa0c31adeca72af
Sumer Cip <sumerc@gmail.com>
2020-05-28 13:21:58 +0300

    update memspike templates

--- generate_data.py
+++ generate_data.py
@@ -10,7 +10,7 @@ print(Comment.objects.count())
 
 new_comments = []
 for i in range(150):
-    new_comments.append(Comment(user=u, comment='Oh! Blackfire is cool!'))
+    new_comments.append(Comment(user=u, comment='Blackfire is cool!'))
     if (i % 100) == 0:
         print(i)
 Comment.objects.bulk_create(new_comments)
